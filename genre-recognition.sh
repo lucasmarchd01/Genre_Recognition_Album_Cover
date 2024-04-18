47280d07a47f81bcbc935eb3cad1e6daac9d2c6f
Lucas <lucas.march@mail.mcgill.ca>
2024-04-18 12:36:21 -0400

    add bash script

---
+++ genre-recognition.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
+#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --time=0-03:00     # DD-HH:MM:SS
+
+module load python/3.10 cuda/12.2 cudnn/8.9.5.29
+export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
+
+SOURCEDIR=~/Genre_Recognition_Album_Cover
+
+# Prepare virtualenv
+source ~/tensorflow/bin/activate
+
+# Prepare data
+mkdir $SLURM_TMPDIR/data
+tar xf ~/mydataset.tar -C $SLURM_TMPDIR/data
+
+# Start training
+python $SOURCEDIR/train.py $SLURM_TMPDIR/data/
