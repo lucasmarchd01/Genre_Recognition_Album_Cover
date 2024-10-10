3cf87d79d5887189e47ce78d0dff764fe9b8711d
Lucas <lucas.march@mail.mcgill.ca>
2024-10-10 14:44:19 -0400

    fix: fix data directory location

--- genre-recognition.sh
+++ genre-recognition.sh
@@ -17,4 +17,4 @@ tar xf ~/2024-10-10.tar -C $SLURM_TMPDIR
 
 # Start training
 tensorboard --logdir=/tmp/logs --host 0.0.0.0 --load_fast false &
-python $SOURCEDIR/train.py --csv_file $SLURM_TMPDIR/data/csv_discogs/final_top_5_discogs.csv --directory $SLURM_TMPDIR/data/
+python $SOURCEDIR/train.py --csv_file $SLURM_TMPDIR/data/csv_discogs/final_top_5_discogs.csv --directory $SLURM_TMPDIR
