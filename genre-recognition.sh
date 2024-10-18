e2af2cf6e9fffdd556b0108afb4595d8f73fea0f
lucasmarchd01 <lucasmarchd01@gmail.com>
2024-10-18 09:32:01 -0400

    refactor: request more time with GPU, change n-trials

--- genre-recognition.sh
+++ genre-recognition.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=0-12:00     # DD-HH:MM:SS
+#SBATCH --time=1-12:00     # DD-HH:MM:SS
 
 module load python/3.10 cuda/12.2 cudnn/8.9.5.29
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
