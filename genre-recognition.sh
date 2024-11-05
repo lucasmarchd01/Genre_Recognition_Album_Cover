b6aea9ca54f8422d5bf4993bfcc5e96cdf91d73f
Lucas <lucas.march@mail.mcgill.ca>
2024-11-05 15:20:16 -0500

    fix(bash): add allocation account

--- genre-recognition.sh
+++ genre-recognition.sh
@@ -3,6 +3,7 @@
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=2-00:00     # DD-HH:MM:SS
+#SBATCH --account=def-ichiro
 
 module load python/3.10 cuda/12.2 cudnn/8.9.5.29
 export XLA_FLAGS=--xla_gpu_cuda_data_dir=$CUDA_HOME
