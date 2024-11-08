8bbb230cec6d45db80a2175a73a60cdac0d6925f
Lucas <lucas.march@mail.mcgill.ca>
2024-11-08 09:27:07 -0500

    fix: add memory

--- scripts/genre-recognition.sh
+++ scripts/genre-recognition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
+#SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=2-00:00     # DD-HH:MM:SS
 #SBATCH --account=def-ichiro
 
