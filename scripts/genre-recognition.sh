0184503e54b6174443012a818aa9ff739b88eb17
Lucas <lucas.march@mail.mcgill.ca>
2024-11-12 10:13:55 -0500

    fix: change GPU type

--- scripts/genre-recognition.sh
+++ scripts/genre-recognition.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1       # Request GPU "generic resources"
+#SBATCH --gres=gpu:v100l:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=125G         # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
 #SBATCH --time=2-00:00     # DD-HH:MM:SS
